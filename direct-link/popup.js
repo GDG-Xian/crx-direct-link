32dba3637cf3753d90bd58725265bda54280f481
greatghoul <greatghoul@gmail.com>
2012-11-13 14:28:01 +0800

    Merge from devel

--- direct-link/popup.js
+++ direct-link/popup.js
@@ -2,6 +2,8 @@ var app = chrome.extension.getBackgroundPage();
 
 $(function() {
     var $show_enabled = $('#show-enabled-only');
+    var $skip_safe_check = $('#skip-safe-check');
+
     // 选中已经启用的域名
     app.iter_enabled_domains(function(domain_name) {
         $('.domain[value="' + domain_name + '"]').prop('checked', true); 
@@ -20,5 +22,12 @@ $(function() {
         var flag = $show_enabled.prop('checked');
         $('.domain-list li .domain').not(':checked').parents('li').toggle(!flag);
     });
+    
+    $skip_safe_check.click(function() {
+        var flag = $skip_safe_check.prop('checked');
+        app.toggle_safe_check(flag); 
+    });
+
+    $skip_safe_check.prop('checked', app.get_option('skip-safe-check') === 'true');
 });
 
