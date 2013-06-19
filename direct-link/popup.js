aada6af34ce4345da06757274fcdbba28b43b73b
greatghoul <greatghoul@gmail.com>
2013-06-20 00:43:46 +0800

    移除额外库，使用原生js实现

--- direct-link/popup.js
+++ direct-link/popup.js
@@ -5,9 +5,10 @@ function modeChangeHandler() {
     app.config('mode', this.value);
 }
 
-Array.create(document.getElementsByName('mode')).forEach(function(modeNode) {
-    // 选中当前模式
+var modeNodes = document.getElementsByName('mode');
+for (var i = 0; i < modeNodes.length; i++) {
+    var modeNode = modeNodes[i];
     if (mode === modeNode.value) modeNode.checked = 'checked';
     modeNode.addEventListener('click', modeChangeHandler, false);
-});
+}
 
