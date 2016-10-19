a43a71b4a3816328a9cedc69e35ac1e03a2139ce
greatghoul <greatghoul@gmail.com>
2016-10-19 23:46:39 +0800

    优化切换模式的信息提示

--- direct-link/background.js
+++ direct-link/background.js
@@ -1,53 +1,71 @@
 const RE_SEARCH = /https?\:\/\/(www\.google\.[^\/]+)\/url\?.*/;
-const FILTERS = { urls: ['<all_urls>'], types: ['main_frame'] };
+const FILTERS = { urls: ['<all_urls>'], types: ['main_frame', 'sub_frame'] };
+const LOCALES = {
+    on: {
+        icon: 'icon19-on.png',
+        title: '直连已启用',
+        message: '将自动跳过 Google 安全检查'
+    },
+    off: {
+        icon: 'icon19-off.png',
+        title: '直连已禁用',
+        message: 'Google 安全检查为你保驾护航'
+    }
+};
+
+function isModeOn() {
+    return localStorage['mode'] == 'on';
+}
 
 function getMode() {
-    return localStorage['mode'] || 'AUTO';
+    return isModeOn() ? 'on' : 'off';
 }
 
 function setMode(mode) {
     localStorage['mode'] = mode;
-    setModeIcon(mode);
 }
 
-function setModeIcon(mode) {
-    chrome.browserAction.setIcon({ path: 'icon19-' + mode + '.png' });
+function updatePopup(event) {
+    const locales = LOCALES[getMode()];
+    console.dir(locales);
+
+    chrome.browserAction.setIcon({ path: locales.icon });
+    chrome.browserAction.setTitle({ title: `${locales.title}\n${locales.message}` });
+
+    // Don't show notiffication on installed.
+    if (!event['reason']) {
+        chrome.notifications.create('crx-direct-link', {
+            type: 'basic',
+            iconUrl: locales.icon,
+            title: locales.title,
+            message: locales.message
+        });
+    }
+}
+
+function toggleMode(event) {
+    setMode(isModeOn() ? 'off' : 'on');
+    updatePopup(event);
 }
 
 function extractTargetUrl(sourceUrl) {
     return sourceUrl.match(/(q|url)=([^&]+)/ig) && decodeURIComponent(RegExp.$2)
 }
 
-function isActiveMode(redirectMode, requestMode) {
-    return redirectMode !== 'REDIRECT' && redirectMode == requestMode
-}
-
 function isRedirectUrl(url) {
     return RE_SEARCH.test(url);
 }
 
-// 中转请求控制器
-function dispatcherHandler(requestMode) {
-    return function(details) { 
-        const redirectMode = getMode();
+function directHandler(details) {
+    if (isModeOn() && isRedirectUrl(details.url)) {
+        const targetUrl = extractTargetUrl(details.url);
 
-        if (isActiveMode(redirectMode, requestMode) && isRedirectUrl(details.url)) {
-            const targetUrl = extractTargetUrl(details.url);
+        console.info('Direct link to', targetUrl);
 
-            console.info(redirectMode, '=>', targetUrl);
-            if (redirectMode == 'DIRECT') {
-                // onBeforeRequest use requestBlocking to redirect request directly
-                return { redirectUrl: targetUrl };
-            } else {
-                // onErrorOccurred updates the tab instead
-                console.log('  REASON:'. details.error);
-                return { cancel: true };
-            }
-        }
+        return { redirectUrl: targetUrl };
     }
 }
 
-chrome.webRequest.onErrorOccurred.addListener(dispatcherHandler('AUTO'), FILTERS);
-chrome.webRequest.onBeforeRequest.addListener(dispatcherHandler('DIRECT'), FILTERS, ['blocking']);
-
-setModeIcon(getMode());
+chrome.webRequest.onBeforeRequest.addListener(directHandler, FILTERS, ['blocking']);
+chrome.browserAction.onClicked.addListener(toggleMode);
+chrome.runtime.onInstalled.addListener(updatePopup);
